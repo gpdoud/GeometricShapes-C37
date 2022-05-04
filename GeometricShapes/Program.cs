5ad7d18500fc7c6a834661259dc8c0c72a01859c
Greg Doud <gdoud@gmail.com>
2022-05-04 15:19:25 -0400

    Add project files.

---
+++ GeometricShapes/Program.cs
@@ -0,0 +1,9 @@
+﻿using GeometricShapes;
+
+var q1 = new Quad() { Side1 = 3, Side2 = 4, Side3 = 5, Side4 = 6 };
+
+Console.WriteLine(q1.Perimeter());
+
+var r1 = new Rect() { Side1 = 3, Side2 = 5 };
+
+Console.WriteLine($"{r1.Perimeter()} | {r1.Area()}");
---
+++ GeometricShapes/Rect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricShapes {
+    internal class Rect {
+
+        public int Side1 { get; set; } = 0;
+        public int Side2 { get; set; } = 0;
+
+        public int Perimeter() {
+            return 2 * (Side1 + Side2);
+        }
+
+        public int Area() {
+            return Side1 * Side2;
+        }
+    }
+}
